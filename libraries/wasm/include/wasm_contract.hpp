2dc912ee9f81f3f6fcf14e88ce520ae312190d18
zhangbo-miuNEST <zhangbo@iotee.io>
2018-09-16 16:30:46 +0800

    add wasm engine

--- libraries/wasm/include/wasm_contract.hpp
+++ libraries/wasm/include/wasm_contract.hpp
@@ -1,5 +1,4 @@
 #pragma once
 
-#include "WASM/wasm.h"
+//#include "WASM/wasm.h"
 
-int hello_world();
--- libraries/wasm/src/wasm_contract.cpp
+++ libraries/wasm/src/wasm_contract.cpp
@@ -1,6 +1,32 @@
 #include "wasm_contract.hpp"
+#include <graphene/chain/account_evaluator.hpp>
+#include "wasm_interface.hpp"
 
-int hello_world()
+string smart_contract_deploy_evaluator::construct_smart_contract(const string &bytecode,
+																const contract_addr_type &contract_addr,
+																const string &construct_data,
+																const string &abi_json)
 {
-	return 0;
+	//TODO:
+	return "";
+}
+
+
+string smart_contract_call_evaluator::call_smart_contract(const string &bytecode,
+														   const contract_addr_type &contract_addr,
+														   const string &call_data,
+														   const string &abi_json,
+														   const string &starting_state)
+{
+	//TODO
+
+	wasm_interface wasmif(wavm);
+
+	digest_type   code_id;
+	shared_string code;
+	apply_context context;
+
+	wasmif.apply(code_id, code, context);
+
+	return "";
 }
