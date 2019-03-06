a1708b4671794d8dd442ae92693979be43e0f2b2
zhangbo <zhangbo@iotee.io>
2019-03-06 21:10:49 +0800

    travis: build witness_node only

--- build_travis.sh
+++ build_travis.sh
@@ -9,4 +9,4 @@ elif [[ $TRAVIS_OS_NAME == osx ]]; then
 	cmake -DWASM_ROOT=$HOME/opt/wasm -DLLVM_DIR=$HOME/opt/wasm/lib/cmake/llvm -DCMAKE_CXX_COMPILER=/usr/local/Cellar/llvm/7.0.1/bin/clang++ -DCMAKE_C_COMPILER=/usr/local/Cellar/llvm/7.0.1/bin/clang -DBOOST_ROOT=$HOME/opt/boost -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib -DCMAKE_BUILD_TYPE=Release ..
 fi
 
-make -j4
+make -j4 witness_node
