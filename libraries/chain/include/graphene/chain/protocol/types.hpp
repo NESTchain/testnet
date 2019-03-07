b577953c335a18dae144f8aa66f45c06e163a968
zhangbo <zhangbo@iotee.io>
2019-03-07 12:11:35 +0800

    travis: fix make target names

--- libraries/chain/include/graphene/chain/protocol/types.hpp
+++ libraries/chain/include/graphene/chain/protocol/types.hpp
@@ -159,7 +159,7 @@ namespace graphene { namespace chain {
       transfer_restricted  = 0x08, /**< require the issuer to be one party to every transfer */
 //      disable_force_settle = 0x10, /**< disable force settling */
 //      global_settle        = 0x20, /**< allow the bitasset issuer to force a global settling -- this may be set in permissions, but not flags */
-      disable_confidential = 0x40, /**< allow the asset to be used with confidential transactions */
+//      disable_confidential = 0x40, /**< allow the asset to be used with confidential transactions */
       witness_fed_asset    = 0x80, /**< allow the asset to be fed by witnesses */
       committee_fed_asset  = 0x100 /**< allow the asset to be fed by the committee */
    };
