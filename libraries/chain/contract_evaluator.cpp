368ab98b1ca5f91b8314af4360192b6c7b2d2971
zhangbo <zhangbo@iotee.io>
2019-03-17 18:43:28 +0800

    rename GXB/gxc to NVM

--- libraries/chain/contract_evaluator.cpp
+++ libraries/chain/contract_evaluator.cpp
@@ -15,6 +15,9 @@
 
     You should have received a copy of the GNU General Public License
     along with gxb-core.  If not, see <http://www.gnu.org/licenses/>.
+
+
+    Copyright (c) 2018- μNEST Foundation, and contributors.
  */
 
 #include <fc/smart_ref_impl.hpp>
